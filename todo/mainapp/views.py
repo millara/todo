7af1ef1fb6b5c7ced3864bb35913a8300a23cca9
Alex Millar <millaralex@gmail.com>
2016-04-05 01:39:28 +0800

    cleaned up unneeded files and code

--- todo/mainapp/views.py
+++ todo/mainapp/views.py
@@ -22,80 +22,12 @@ class JobCreateView(CreateView):
     #template_name = "job_create_form.html"
 
 
+class JobDelete(DeleteView):
+    model = Job
+    def get_success_url(self):
+        return reverse('mainapp:jobs')
+
+
 def home(request):
     hello = "hello view"
     return HttpResponse(hello)
-
-
-def job_create(request):
-    form = AddJobForm()
-    context = {
-        "form": form,
-    }
-    return render(request, "add_job_form.html", context)
-
-
-# def create_job(request):
-#     temp_var = [0, 1, 2, 3, 4, 5]
-#     template = loader.get_template('mainapp/todolist.html')
-#     context = {'temp_var': temp_var}
-#     return HttpResponse(template.render(context, request))
-
-
-@csrf_protect
-def create_job_test(request):
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        job = Job()
-
-        form = CreateJobForm(request.POST, instance=job)
-
-        if form.isvalid():
-            # process the data in form.cleaned_data as required.
-            # if is_valid() is True, we’ll now be able to find all
-            # the validated form data in its cleaned_data attribute.
-            # We can use this data to update the database or do
-            # other processing before sending an HTTP redirect to
-            # the browser telling it where to go next.
-            # redirect to a new URL:
-
-            job.save()
-
-            # return HttpResponseRedirect('/display_jobs/')
-            return render(request, 'create_job.html', {'form: form'})
-    # if a GET (or any other method) we'll create a blank form
-    else:
-        form = CreateJobForm()
-
-    return render(request, 'mainapp/create_job.html', {'form': form})
-
-
-
-# def display_jobs(request):
-#     # get list of jobs then display them
-#     # if request == POST???? do i need to do this like in the create
-#     # job test function above in order to save the data?
-#     if request.method == "POST":
-#         form = AddJobForm(request.POST)
-#         if form.is_valid():
-#             form.save(commit=True)
-#         else:
-#             print(form.errors)
-
-#     job_list = Job.objects.all()
-#     template = loader.get_template('display_jobs.html')
-#     context = {'job_list': job_list}
-#     return HttpResponse(template.render(context, request))
-
-
-def current_datetime(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
-
-# Subclass the generic DeleteViewleteView
-class JobDelete(DeleteView):
-    model = Job
-    #template_name = 'delete_job.html'
-    def get_success_url(self):
-        return reverse('mainapp:jobs')
